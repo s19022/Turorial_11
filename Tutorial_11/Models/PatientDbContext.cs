b8ec94772a27a546f66d29ee84ad714dffcd7750
Volodymyr Dovhyi <s19022@pjwstk.edu.pl>
2020-05-22 13:48:18 +0300

    init

---
+++ Tutorial_11/Models/PatientDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tutorial_11.Models
+{
+    public class PatientDbContext : DbContext
+    {
+        public  DbSet<Patient> Patient { get; set; }
+
+        public PatientDbContext(DbContextOptions options): base(options)
+        {
+
+        }
+    }
+}
