9e16acd5d8832fd85311aebd7e28e7077d009d9b
Volodymyr Dovhyi <s19022@pjwstk.edu.pl>
2020-05-22 13:53:00 +0300

    Added controller

---
+++ Tutorial_11/Controllers/PatientController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Tutorial_11.Models;
+
+namespace Tutorial_11.Controllers
+{
+    [Route("api/patient")]
+    [ApiController]
+    public class PatientController : ControllerBase
+    {
+        private readonly PatientDbContext _context;
+
+        public PatientController(PatientDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetPatients()
+        {
+            return Ok(_context.Patient.ToList());
+        }
+    }
+}
